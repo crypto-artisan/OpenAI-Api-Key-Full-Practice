2f845ab390fa2c5fac6210edca12c9aa947b1d56
hightecular95908 <ronnykutter95908@gmail.com>
2024-02-09 16:17:33 +1000

    main.py uncommented

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# from langchain_openai.llms import OpenAI
-# import dotenv
-# dotenv.load_dotenv()
-# # Before executing the following code, make sure to have
-# # your OpenAI key saved in the “OPENAI_API_KEY” environment variable.
-# llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0.9)
-# text = "Suggest a personalized workout routine for someone looking to improve cardiovascular endurance and prefers outdoor activities."
-# print(llm(text))
+from langchain_openai.llms import OpenAI
+import dotenv
+dotenv.load_dotenv()
+# Before executing the following code, make sure to have
+# your OpenAI key saved in the “OPENAI_API_KEY” environment variable.
+llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0.9)
+text = "Suggest a personalized workout routine for someone looking to improve cardiovascular endurance and prefers outdoor activities."
+print(llm(text))
 ##################################image generation#####################################
 # import openai
 # from packaging import version
